85029961acd8e107d35cf22fbec885d20d0fc97e
Denys Kochetov <denkoch1355@gmail.com>
2025-04-03 13:36:16 +0300

    [Add, Update] decorators, logs

--- src/test/java/decorator/elements/Input.java
+++ src/test/java/decorator/elements/Input.java
@@ -1,7 +1,9 @@
 package decorator.elements;
 
+import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.WebElement;
 
+@Slf4j
 public class Input extends Element {
 
     public Input(WebElement webElement) {
@@ -11,5 +13,6 @@ public class Input extends Element {
     public void enterText(String text) {
         waitElementToBeClickable();
         webElement.sendKeys(text);
+        log.info("Entered text is: {}", text);
     }
 }
